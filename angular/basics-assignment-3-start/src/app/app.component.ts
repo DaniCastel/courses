fcbb372a4de78f61e3b889d3b655e093c4bda8bb
Daniela Castellanos <ldcastellanosa@gmail.com>
2021-04-14 20:00:10 -0500

    assignment-3

--- angular/basics-assignment-3-start/src/app/app.component.ts
+++ angular/basics-assignment-3-start/src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { timestamp } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  displayDetails = false;
+  buttonClicks = [];
+  toggleDetails() {
+    this.buttonClicks.push(Date.now());
+    this.displayDetails = !this.displayDetails;
+  }
 }
