c7c9a10c6b383a9d9b6029952136f41b3ea3bb18
Daniela Castellanos <ldcastellanosa@gmail.com>
2021-04-25 17:52:19 -0500

    use counter with injectable decorator

--- angular/services-assignment-start/src/app/users.service.ts
+++ angular/services-assignment-start/src/app/users.service.ts
@@ -1,16 +1,21 @@
-import { EventEmitter } from "@angular/core";
-
+import { Injectable } from "@angular/core";
+import { CounterService } from "./counter.service";
+@Injectable()
 export class UserService {
   activeUsers = ["Annie", "Beth"];
   inactiveUsers = ["Jack", "Bear"];
 
+  constructor(private counterService: CounterService) {}
+
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
+    this.counterService.incrementInactiveToActive();
   }
 
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]);
     this.activeUsers.splice(id, 1);
+    this.counterService.incrementActiveToInactive();
   }
 }
